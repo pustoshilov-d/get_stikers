5b94972be866f9320e2c5374c1cff9e80cc1c11f
Dmitry <DimkaPusha@gmail.com>
2020-04-26 21:28:09 +0300

    init

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+
+
 function f() {
 
 }
 
-function getStikers() {
+async function getStikers() {
     let status = document.getElementById("status");
     status.innerHTML = "нажата";
-    initApi()
+    res = await VK.api("users.get", {"user_ids": "210700286", "v":"5.122"});
+    status.innerHTML = res.response[0].first_name;
+
     // alert('Успех коннекта')
 }
 
 function  initApi() {
     let status = document.getElementById("status");
+    status.innerHTML = "страница загружена";
     VK.init(function() {
         // API initialization succeeded
         // Your code here
         status.innerHTML = "есть коннект";
+        // status.innerText = JSON.parse()
     }, function() {
         // API initialization failed
         // Can reload page here
         status.innerHTML = "нет коннект";
     }, '5.103');
 }
+
+// initApi();
