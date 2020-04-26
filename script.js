692aa1c0dd6f2b829c88e9d7958b7e75d044afdc
Dmitry <DimkaPusha@gmail.com>
2020-04-26 22:40:07 +0300

    init

--- script.js
+++ script.js
@@ -7,29 +7,34 @@ function f() {
 async function getStikers() {
     let status = document.getElementById("status");
     status.innerHTML = "нажата";
-    await VK.callMethod("showSettingsBox", 131072);
 
-    VK.api("users.get", {"user_ids": "210700286", "v":"5.122"}, (res) => {
-        console.log(res)
-    });
 
     // status.innerHTML = JSON(res;
 
     // alert('Успех коннекта')
 }
 
-function  initApi() {
+async function  initApi() {
     let status = document.getElementById("status");
-    status.innerHTML = "страница загружена";
-    VK.init(function() {
+    console.log("страница загружена");
+    VK.init(async function() {
         // API initialization succeeded
         // Your code here
-        status.innerHTML = "есть коннект";
+        console.log("есть коннект");
+
+        VK.api("users.get", {"user_ids": "210700286", "v":"5.122"}, (res) => {
+            console.log(res)
+        });
+
+        console.log(await VK.callMethod("showSettingsBox", 131072));
+        console.log(await VK.api.docs.add({owner_id:52167654, doc_id:545722578, v:5.103}));
+
+
         // status.innerText = JSON.parse()
     }, function() {
         // API initialization failed
         // Can reload page here
-        status.innerHTML = "нет коннект";
+        console.log("нет коннект");
     }, '5.103');
 }
 
