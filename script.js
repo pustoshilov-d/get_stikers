044489c5eff05e076039dc7618c7b77c2ddd4d5d
Dmitry <DimkaPusha@gmail.com>
2020-04-28 02:21:55 +0300

    init

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let token = "";
 let captcha_sid = null;
-let list = [545722298, 545722301, 545722340, 545722343, 545722345, 545722351, 545722410, 545722413, 545722415, 545722420, 545722422, 545722425, 545722427
+let docs_list = [545722298, 545722301, 545722340, 545722343, 545722345, 545722351, 545722410, 545722413, 545722415, 545722420, 545722422, 545722425, 545722427
     , 545722428, 545722432, 545722434, 545722441, 545722446, 545722449, 545722557, 545722558, 545722561, 545722565, 545722567, 545722571, 545722574, 545722575
     , 545722578, 545722582, 545722585, 545722586, 545722587, 545722949, 545722958, 545722961];
 
@@ -22,16 +22,16 @@ async function get_stikers(token, captcha_key) {
 
         console.log('капча дата', captcha_key, captcha_sid);
 
-        let code = "var list = ["+ list.toString() + "];\n" +
+        let code = "var docs_list = ["+ docs_list.toString() + "];\n" +
             "var i = 0;\n" +
             "var captcha_key = \"" + captcha_key + "\";\n" +
             "var captcha_sid = \"" + captcha_sid +"\";\n" +
             "if (captcha_key != \"null\"){\n" +
-            "  API.docs.add({owner_id:52167654, doc_id:list[i], captcha_key: captcha_key, captcha_sid: captcha_sid});\n" +
+            "  API.docs.add({owner_id:52167654, doc_id:docs_list[i], captcha_key: captcha_key, captcha_sid: captcha_sid});\n" +
             "  i = i + 1;\n" +
             "};\n" +
-            "while (i != list.length-1) {\n" +
-            "  API.docs.add({owner_id:52167654, doc_id:list[i]});\n" +
+            "while (i != docs_list.length-1) {\n" +
+            "  API.docs.add({owner_id:52167654, doc_id:docs_list[i]});\n" +
             "  i = i + 1;\n" +
             "};\n" +
             "return \"Чё-то добавлено\";";
@@ -41,7 +41,7 @@ async function get_stikers(token, captcha_key) {
         await vkBridge.send("VKWebAppCallAPIMethod", {"method": "execute", "request_id": "ex", "params": {"code": code, "v":5.103, "access_token":token}})
             .then(res => {
                 console.log('Результат выполнения', res);
-                console.log("добавлены оставш: ", list.toString());
+                console.log("добавлены оставш: ", docs_list.toString());
                 let status = document.getElementById("status");
                 status.innerText = "Стикеры загружены. Проверяй";
                 link.removeAttribute("hidden");
@@ -60,9 +60,9 @@ async function get_stikers(token, captcha_key) {
                     let status = document.getElementById("status");
                     status.innerText = "Введи капчу";
 
-                    let cur_doc = parseInt(e.error_data.error_reason.request_params["doc_id"]);
-                    console.log("добавлены: ", list.slice(0,list.indexOf(cur_doc)).toString());
-                    list = list.slice(list.indexOf(cur_doc));
+                    let cur_doc = parseInt(e.error_data.error_reason.request_params[1].value);
+                    console.log("добавлены: ", docs_list.slice(0,docs_list.indexOf(cur_doc)).toString());
+                    docs_list = docs_list.slice(docs_list.indexOf(cur_doc));
 
                     capcha.removeAttribute("hidden");
                 }
